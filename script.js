b9b161247ec361ea336402a6409710606a43d287
warpasylum <chthonster@gmail.com>
2022-05-12 15:01:33 -0400

    Add loop to play game five times
    Wrote function game() which loops the game five times. After the fifth
    game ends, it returns undefined, unclear as to why.

--- script.js
+++ script.js
@@ -12,25 +12,37 @@
 // }
 
 // Player plays rock
-// Computer plays a random selectino of rock, paper, or scissors
+// Generate random selection of rock, paper, or scissors
+// Computer plays a random selection of rock, paper, or scissors
 // If computer plays paper, computer wins
 // If computer plays scissors, computer loses
 // If computer plays rock, the game ends in a tie
 
-let RPS = ['rock', 'paper', 'scissors'];
-let randomRPS = RPS[Math.floor(Math.random() * RPS.length )]
+const rps = ["rock", "paper", "scissors"];
+function computerPlay() {
+  return rps[Math.floor(Math.random() * rps.length)];
+}
 
 function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay().toLowerCase();
   if (computerSelection == "scissors") {
-    return ("You win! Rock beats Scissors");
+    return "You win! Rock beats Scissors";
   } else if (computerSelection == "paper") {
-    return ("You Lose! Paper beats Rock");
+    return "You Lose! Paper beats Rock";
   } else if (computerSelection == "rock") {
-    return ("You tie.");
+    return "You tie.";
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = randomRPS;
+const playerSelection = "RoCK";
+const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+  }
+}
+
+
 
