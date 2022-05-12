30e5f1e02a1cace631606f1feebfdc0b3f65f26c
warpasylum <chthonster@gmail.com>
2022-05-11 21:40:20 -0400

    Randomize computer selection
    Randomized the computer's selection with a selection from an array.
    Replaced computerPlay(); with the string randomRPS.

--- script.js
+++ script.js
@@ -3,14 +3,34 @@
 // Else
 // If computer plays paper, computer wins
 
+// function playRound(playerSelection, computerSelection) {
+//   if (computerSelection == "scissors") {
+//     return ("You win! Rock beats Scissors");
+//   } else if (computerSelection == "paper") {
+//     return ("You Lose! Paper beats Rock");
+//   }
+// }
+
+// Player plays rock
+// Computer plays a random selectino of rock, paper, or scissors
+// If computer plays paper, computer wins
+// If computer plays scissors, computer loses
+// If computer plays rock, the game ends in a tie
+
+let RPS = ['rock', 'paper', 'scissors'];
+let randomRPS = RPS[Math.floor(Math.random() * RPS.length )]
+
 function playRound(playerSelection, computerSelection) {
   if (computerSelection == "scissors") {
     return ("You win! Rock beats Scissors");
   } else if (computerSelection == "paper") {
     return ("You Lose! Paper beats Rock");
+  } else if (computerSelection == "rock") {
+    return ("You tie.");
   }
 }
 
 const playerSelection = "rock";
-const computerSelection = "computerPlay()";
+const computerSelection = randomRPS;
 console.log(playRound(playerSelection, computerSelection));
+
