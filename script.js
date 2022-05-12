1e84d172fc88747ef80a2a7de7422da2495284c6
warpasylum <chthonster@gmail.com>
2022-05-12 17:40:40 -0400

    Add prompt on page load
    Prompt now appears on page load. Took awhile to figure out why
    userPlay() was being called twice, had to rewrite it as an expression
    instead. Dumb mistake. Still new I guess.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Player plays rock
 // If computer plays scissors computer loses
 // Else
@@ -19,30 +20,47 @@
 // If computer plays rock, the game ends in a tie
 
 const rps = ["rock", "paper", "scissors"];
+
+// Is computerPlay() being called on load? Does it matter? Should it be a function expression instead?
+
 function computerPlay() {
   return rps[Math.floor(Math.random() * rps.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  playerSelection = userPlay().toLowerCase();
   computerSelection = computerPlay().toLowerCase();
-  if (computerSelection == "scissors") {
-    return "You win! Rock beats Scissors";
-  } else if (computerSelection == "paper") {
-    return "You Lose! Paper beats Rock";
-  } else if (computerSelection == "rock") {
+  if (computerSelection == "scissors" && playerSelection == "rock") {
+    return "You win! Rock beats Scissors.";
+  } else if (computerSelection == "scissors" && playerSelection == "paper") {
+    return "You lose! Scissors beats Paper.";
+  } else if (computerSelection == "scissors" && playerSelection == "scissors") {
+    return "You Tie.";
+  } else if (computerSelection == "paper" && playerSelection == "rock") {
+    return "You Lose! Paper beats Rock.";
+  } else if (computerSelection == "paper" && playerSelection == "paper") {
+    return "You Tie.";
+  } else if (computerSelection == "paper" && playerSelection == "scissors") {
+    return "You Win! Scissors beats Paper.";
+  } else if (computerSelection == "rock" && playerSelection == "rock") {
     return "You tie.";
+  } else if (computerSelection == "rock" && playerSelection == "paper") {
+    return "You win! Paper beats Rock.";
+  } else if (computerPlay == "rock" && playerSelection == "scissors") {
+    return "You lose! Rock beats Scissors.";
   }
 }
 
-const playerSelection = "RoCK";
+//  Wrote userPlay() as a function expression to ensure it does not get called on load, and only gets called once playRound() is called
+
+const userPlay = function () {
+  return prompt("Rock, Paper, or Scissors?");
+};
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound());
 
 function game() {
   for (let i = 0; i < 5; i++) {
     console.log(playRound());
   }
 }
-
-
-
