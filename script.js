c0fa74a78de544b46c44ff819f26f407ee08ff7d
warpasylum <chthonster@gmail.com>
2022-06-20 03:18:25 -0400

    Create UI for Rock, Paper, Scissors game
    Created DOM tree for UI. Added three buttons to select a choice. Added
    function gameScore() and finalScore() to show a winner after 5 wins.
    Added functions to disable buttons upon a win and re-enable buttons when
    game is reset. Added function newButton() that resets the game when
    there is a winner. Added function gameScore() to declare a winner. Added
    some CSS to make it more presentable.

--- script.js
+++ script.js
@@ -1,53 +1,116 @@
-"use strict";
+'use strict';
 
-const rps = ["rock", "paper", "scissors"];
+const rockButton = document.querySelector('#rockButton');    
+rockButton.addEventListener('click', function (e) {
+  (results.textContent = playRound('rock')),
+    ((score.textContent = finalScore()), (endGame.textContent = gameScore()));
+});
 
-// Wrote userPlay() as a function expression to ensure it does not get called
-// on load, and only gets called once playRound() is called. Yes, I'm dumb.
+const paperButton = document.querySelector('#paperButton');
+paperButton.addEventListener('click', function (e) {
+  (results.textContent = playRound('paper')),
+    ((score.textContent = finalScore()), (endGame.textContent = gameScore()));
+});
 
-const userPlay = function () {
-  return prompt("Rock, Paper, or Scissors?");
-};
-const computerSelection = computerPlay();
-console.log(playRound());
+const scissorsButton = document.querySelector('#scissorsButton');
+scissorsButton.addEventListener('click', function (e) {
+  (results.textContent = playRound('scissors')),
+    ((score.textContent = finalScore()), (endGame.textContent = gameScore()));
+});
 
-// Is computerPlay() being called on load? Does it matter? Should it be a function
-// expression instead?
+const container = document.querySelector('#container');
 
-function computerPlay() {
+const results = document.createElement('results');
+const score = document.createElement('score');
+const endGame = document.createElement('endGame');
+
+container.appendChild(results);
+container.appendChild(score);
+container.appendChild(endGame);
+
+const rps = ['rock', 'paper', 'scissors'];
+
+const computerPlay = function () {
   return rps[Math.floor(Math.random() * rps.length)];
-}
+};
+
+let winRS = 'You win! Rock beats Scissors.';
+let winSP = 'You Win! Scissors beats Paper.';
+let winPR = 'You win! Paper beats rock.';
+let loseSP = 'You Lose! Scissors beats paper';
+let loseRS = 'You lose! Rock beats scissors.';
+let losePR = 'You lose! Paper beats rock.';
+let draw = 'You tie.';
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = userPlay().toLowerCase(); // Learned new toLowerCase() syntax. Don't forget!!!
-  computerSelection = computerPlay().toLowerCase();
-  if (computerSelection == "scissors" && playerSelection == "rock") {
-    return "You win! Rock beats Scissors.";
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    return "You lose! Scissors beats Paper.";
-  } else if (computerSelection == "scissors" && playerSelection == "scissors") {
-    return "You Tie.";
-  } else if (computerSelection == "paper" && playerSelection == "rock") {
-    return "You Lose! Paper beats Rock.";
-  } else if (computerSelection == "paper" && playerSelection == "paper") {
-    return "You Tie.";
-  } else if (computerSelection == "paper" && playerSelection == "scissors") {
-    return "You Win! Scissors beats Paper.";
-  } else if (computerSelection == "rock" && playerSelection == "rock") {
-    return "You tie.";
-  } else if (computerSelection == "rock" && playerSelection == "paper") {
-    return "You win! Paper beats Rock.";
-  } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    return "You lose! Rock beats Scissors.";
+  computerSelection = computerPlay();
+  if (computerSelection == 'scissors' && playerSelection == 'rock') {
+    playerScore++;
+    return winRS;
+  } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+    computerScore++;
+    return loseSP;
+  } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+    computerScore++;
+    return losePR;
+  } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+    playerScore++;
+    return winSP;
+  } else if (computerSelection == 'rock' && playerSelection == 'paper') {
+    playerScore++;
+    return winPR;
+  } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+    computerScore++;
+    return loseRS;
+  } else if (computerSelection === playerSelection) {
+    return draw;
   }
 }
 
-// Loops game 5 times by counting up from 0
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound());
-  }
+function finalScore() {
+  return 'Player: ' + playerScore + ' ' + 'Computer ' + computerScore;
 }
 
+function disableBtn() {
+    document.getElementById("rockButton").disabled = true;
+    document.getElementById("paperButton").disabled = true;
+    document.getElementById("scissorsButton").disabled = true;
+  }
+  
+  function enableBtn() {
+    document.getElementById("rockButton").disabled = false;
+    document.getElementById("paperButton").disabled = false;
+    document.getElementById("scissorsButton").disabled = false;
+  }
+  
+  function finishGame() {
+    playerScore = 0;
+    computerScore = 0;
+    score.innerHTML = ``;
+    endGame.innerHTML = ``;
+    results.innerHTML = ``;
+  }
 
+  const resetButton = document.createElement('button');
+  
+  function newButton() { // Appends or unhides the resetButton element, hides it upon click, and re-enables other buttons
+    reset.appendChild(resetButton);
+    resetButton.innerHTML = 'Reset Game';
+    resetButton.style.visibility = 'visible';
+    resetButton.addEventListener('click', function () {
+      resetButton.style.visibility = 'hidden', finishGame(), enableBtn(); 
+    });
+  }
+  
+  function gameScore() {
+    if (playerScore === 5) {
+      newButton(), disableBtn();
+      return 'You win the game.';
+    } else if (computerScore === 5) {
+      newButton(), disableBtn();
+      return 'Computer wins. You lose the game.';
+    } 
+  }
