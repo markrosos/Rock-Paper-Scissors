f8ffa65f654ae56a12d107d596e64aa37e80bcc0
warpasylum <chthonster@gmail.com>
2022-06-21 05:53:16 -0400

    Remove redundant strings
    Removed some redundant strings showing text upon winning and replaced
    them with strings 'win' and 'lose' inside of the playRound() function.

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 'use strict';
 
-let winRS = 'You win! Rock beats Scissors.';
-let winSP = 'You Win! Scissors beats Paper.';
-let winPR = 'You win! Paper beats rock.';
-let loseSP = 'You Lose! Scissors beats paper';
-let loseRS = 'You lose! Rock beats scissors.';
-let losePR = 'You lose! Paper beats rock.';
-let draw = 'You tie.';
-
 let computerScore = 0;
 let playerScore = 0;
 
@@ -19,34 +11,37 @@ const computerPlay = () => {
 
 const playRound = function (playerSelection, computerSelection) {
   computerSelection = computerPlay();
+  let lose = `You lose! ${computerSelection} beats ${playerSelection}`;
+  let win = `You win! ${playerSelection} beats ${computerSelection}`;
+  let draw = 'You tie.';
   if (computerSelection == 'scissors' && playerSelection == 'rock') {
     playerScore++;
-    return winRS;
+    return win;
   } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
     computerScore++;
-    return loseSP;
+    return lose;
   } else if (computerSelection == 'paper' && playerSelection == 'rock') {
     computerScore++;
-    return losePR;
+    return lose;
   } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
     playerScore++;
-    return winSP;
+    return win;
   } else if (computerSelection == 'rock' && playerSelection == 'paper') {
     playerScore++;
-    return winPR;
+    return win;
   } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
     computerScore++;
-    return loseRS;
+    return lose;
   } else if (computerSelection === playerSelection) {
     return draw;
   }
 };
 
-const finalScore = () => {
+const finalScore = () => { // Displays score after each round
   return `Player: ${playerScore}\u00A0\u00A0\u00A0 Computer: ${computerScore}`; // Strings between backticks = template literal. \u00A0 (Non Breaking Whitespace?) is ugly, but works?
 };
 
-const gameResult = () => {
+const gameResult = () => { // Shows winner or loser of game, shows reset button, disables top buttons
   if (playerScore === 5) {
     resetBtn(), disableBtn();
     return 'You win the game.';
@@ -54,12 +49,11 @@ const gameResult = () => {
     resetBtn(), disableBtn();
     return 'Computer wins. You lose the game.';
   }
-}
+};
 
 const resetButton = document.createElement('button'); // Has no class name until given one?
 
-const resetBtn = () => {
-  // Appends or unhides the resetButton element, hides it upon click, and re-enables other buttons
+const resetBtn = () => {  // Appends and unhides the resetButton element, hides it upon click, and re-enables other buttons
   reset.appendChild(resetButton);
   resetButton.className = 'resetButton'; // Gave element class name to style it
   resetButton.innerHTML = 'Reset Game';
@@ -67,48 +61,48 @@ const resetBtn = () => {
   resetButton.addEventListener('click', () => {
     (resetButton.style.visibility = 'hidden'), finishGame(), enableBtn();
   });
-}
+};
 
-const finishGame = () => {
+const finishGame = () => { // Resets scores
   playerScore = 0;
   computerScore = 0;
   score.innerHTML = ``;
   endGame.innerHTML = ``;
   results.innerHTML = ``;
-}
+};
 
 const disableBtn = () => {
   document.getElementById('rockButton').disabled = true;
   document.getElementById('paperButton').disabled = true;
   document.getElementById('scissorsButton').disabled = true;
-}  
+};
 
 const enableBtn = () => {
   document.getElementById('rockButton').disabled = false;
   document.getElementById('paperButton').disabled = false;
   document.getElementById('scissorsButton').disabled = false;
-}  
+};
 
 const rockButton = document.querySelector('#rockButton');
 rockButton.addEventListener('click', () => {
   (results.textContent = playRound('rock')),
     (score.textContent = finalScore()),
     (endGame.textContent = gameResult());
-});    
+});
 
 const paperButton = document.querySelector('#paperButton');
 paperButton.addEventListener('click', () => {
   (results.textContent = playRound('paper')),
     (score.textContent = finalScore()),
     (endGame.textContent = gameResult());
-});    
+});
 
 const scissorsButton = document.querySelector('#scissorsButton');
 scissorsButton.addEventListener('click', () => {
   (results.textContent = playRound('scissors')),
     (score.textContent = finalScore()),
     (endGame.textContent = gameResult());
-});    
+});
 
 const container = document.querySelector('#container');
 
